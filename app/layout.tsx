f2f1cd74f1af53ae52f7dc988a9d1e3ebf9d70dc
Andri10dev <andri10.dev@gmail.com>
2023-10-31 14:51:00 +0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,22 @@ import './globals.css'
 import 'tw-elements/dist/css/tw-elements.min.css'
 import Head from 'next/head'
 
-import Favicon from './icon/android-chrome-512x512.png'
+import Favicon from '../public/favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Linqkeun',
-  description: 'Manage your business with ease with Linqkeun, a mini ERP app that helps you with',
-  icons: {
-    icon: "./favicon.ico",
-    apple: './favicon.ico'
-  },
-  twitter: {
-    images: './favicon.ico'
-  }
-}
+// export const metadata: Metadata = {
+//   title: 'Linqkeun',
+//   description: 'Manage your business with ease with Linqkeun, a mini ERP app that helps you with',
+//   icons: {
+//     icon: "./favicon.ico",
+//     apple: './favicon.ico'
+//   },
+//   twitter: {
+//     images: './favicon.ico'
+//   },
+
+// }
 
 export default function RootLayout({
   children,
@@ -27,25 +28,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link
-          rel="icon"
-          href="./icon/android-chrome-512x512.png"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
+      <head>
 
-        <link
-          rel="apple-touch-icon"
-          href="./icon/android-chrome-512x512.png"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Manage your business with ease with Linqkeun, a mini ERP app that helps you with" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <title>Linqkeun</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png" />
+        <link rel="shortcut-icon" href="../public/favicon.ico" />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
+        <link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
+
   )
 }
